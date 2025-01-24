74901154b92791d5a72b917a0a0310aa5fdebb33
Niraj Kadam <nirajkadam1762@gmail.com>
2025-01-25 02:40:19 +0530

    Update categoryController.js

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,12 @@
-import { getAllCategories, createCategory, updateCategory, deleteCategory, countCategories ,getCategoryById} from "../models/category.js";
+import {
+  getAllCategories,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  countCategories,
+  getCategoryById,
+} from "../models/category.js";
+import { getProductsByCategoryId, deleteProductsByCategoryId } from "../models/product.js";
 
 export const listCategories = async (req, res) => {
   const page = parseInt(req.query.page) || 1;
@@ -7,7 +15,7 @@ export const listCategories = async (req, res) => {
 
   const categories = await getAllCategories(limit, offset);
   const total = await countCategories();
-  res.render('category', {
+  res.render("category", {
     category: categories,
     currentPage: page,
     totalPages: Math.ceil(total / limit),
@@ -17,38 +25,40 @@ export const listCategories = async (req, res) => {
 export const addCategory = async (req, res) => {
   const { name } = req.body;
   await createCategory(name);
-  res.redirect('/categories');
+  res.redirect("/categories");
 };
 
 export const showEditCategoryForm = async (req, res) => {
   const { id } = req.params;
   const category = await getCategoryById(id);
-  res.render('updateCategory', { category });
+  res.render("updateCategory", { category });
 };
 
 export const editCategory = async (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
   await updateCategory(id, name);
-  res.redirect('/categories');
+  res.redirect("/categories");
 };
 
 export const removeCategory = async (req, res) => {
   const { id } = req.params;
   try {
-    await deleteCategory(id);
-    res.redirect('/categories');
-  } catch (error) {
-    if (error.code === 'ER_ROW_IS_REFERENCED_2') {
-      res.status(400).send({ message: 'Cannot delete this category as it is associated with existing products.' });
-    } else {
-      console.error('Error deleting category:', error);
-      res.status(500).send({ message: 'An error occurred while deleting the category.' });
+    const products = await getProductsByCategoryId(id);
+
+    if (products.length > 0) {
+
+      await deleteProductsByCategoryId(id);
     }
+
+    await deleteCategory(id);
+    res.redirect("/categories");
+  } catch (error) {
+    console.error("Error deleting category:", error);
+    res.status(500).send({ message: "An error occurred while deleting the category." });
   }
 };
 
 export const showAddCategoryForm = (req, res) => {
-  res.render('addCategory'); 
+  res.render("addCategory");
 };
-
